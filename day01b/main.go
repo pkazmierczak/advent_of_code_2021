bb7716669e00ff744a56d86c09c74e566b0a0a01
Piotr Kazmierczak <m@piotr.is>
2021-12-02 22:23:38 +0100

    simplified

--- day01b/main.go
+++ day01b/main.go
@@ -7,6 +7,24 @@ import (
 	"strconv"
 )
 
+func day01(lines []int) int {
+	increase, last := 0, 0
+
+	for idx := 0; idx < len(lines)-2; idx++ {
+		num1 := lines[idx]
+		num2 := lines[(idx+1)%len(lines)]
+		num3 := lines[(idx+2)%len(lines)]
+		sum := num1 + num2 + num3
+		if last != 0 {
+			if sum > last {
+				increase++
+			}
+		}
+		last = sum
+	}
+	return increase
+}
+
 func readLines(path string) []int {
 	file, _ := os.Open(path)
 	defer file.Close()
