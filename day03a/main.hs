1bce7bb136b17dfecc3468943632bbc2a0b4894a
Piotr Kazmierczak <m@piotr.is>
2021-12-03 22:54:19 +0100

    simplified

--- day03a/main.hs
+++ day03a/main.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Arrow ((&&&))
-import Data.List (group, sort)
 import Data.Text (pack, splitOn, unpack)
 import System.IO
 
@@ -14,23 +12,26 @@ vertical ([] : _) = []
 vertical ([x] : xs) = [joinHead ([x] : xs)]
 vertical xs = joinHead xs : vertical (map tail xs)
 
---couldn't figure out how to implement this. I found this magical answer
---https://stackoverflow.com/a/62010996/609972 but don't really understand how
---Control.Arrow works... This really motivates me to read more abouto arrows
---though.
-mostCommon = snd . maximum . map (length &&& head) . group . sort
-leastCommon = snd . minimum . map (length &&& head) . group . sort
+count x = length . filter (== x)
+
+mostCommon xs
+  | count '1' xs > count '0' xs = '1'
+  | otherwise = '0'
+
+leastCommon xs
+  | count '1' xs < count '0' xs = '1'
+  | otherwise = '0'
 
 bintodec 0 = 0
 bintodec i = 2 * bintodec (div i 10) + mod i 10
 
 main :: IO ()
 main = do
-  inh <- openFile "day03.txt" ReadMode
+  inh <- openFile "../day03.txt" ReadMode
   contents <- hGetContents inh -- gets a handle and returns a string
   let lines = splitOn "\n" (pack contents)
   let gamma = map mostCommon (vertical (map unpack lines))
   let epsilon = map leastCommon (vertical (map unpack lines))
-  print (bintodec (read epsilon ::Int) * bintodec (read gamma ::Int))
+  print (bintodec (read epsilon :: Int) * bintodec (read gamma :: Int))
 
   hClose inh
